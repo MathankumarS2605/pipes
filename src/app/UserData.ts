be96973d7e4fc3dd8759c90f74aa2611803a48b9
mathankumar.s <mathankumar.s@200411LTP2851>
2023-10-30 10:45:02 +0530

    commit

---
+++ src/app/UserData.ts
@@ -0,0 +1,12 @@
+
+ export class UserData{
+    
+    firstName:string="Mathan";
+    lastName:string="Kumar";
+    company:string="Gavs";
+    location:string="";
+    salary:number=30000;
+    date:Date=new Date();
+
+}
+
